241eff0927d24696480c3d845a9ff2deb446fdd2
Mohammad Eddir <meddir@meddir-VirtualBox>
2017-11-07 23:26:07 -0500

    shit

--- rpn.py
+++ rpn.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
 def calculate(string):
-	pass
+	stack = list()
+	for token in string.split():
+		if token == '+':
+			ar1 = stack.pop()
+			ar2 = stack.pop()
 
+			result =  ar1 + ar2
+			stack.append(result)
+		else:
+			stack.append(int(token))
+	
+
+	return stack.pop()
 def main():
 	while True:
 		calculate(input("rpn calc> "))
--- test_rpn.py
+++ test_rpn.py
@@ -3,7 +3,7 @@ import rpn
 
 class TestBasics(unittest.TestClass):
 	def test_add(self):
-		result =rpn.calculate('1 1 +')
+		result = rpn.calculate('1 1 +')
 		self.assertEqual(2, result)
 
 
